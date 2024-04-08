9c33cb8ad0166a08a9010473f4423977087133f9
Your Name <you@example.com>
2024-04-07 20:30:42 -0700

    Upload modal and functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,24 @@
 "use client"
 
+import useAuthModal from "@/hooks/UseAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
     const onClick = () => {
-        // handle upload later
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        // TODO: check for subscription
+
+        return uploadModal.onOpen();
     };
 
     return ( 
